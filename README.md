61fbbd87c651ece614a45e986bfe29d1b4e195c0
Jim Nervall <nervall@hotmail.com>
2024-09-12 15:33:15 +0200

    Basic setup React, Typescript with Vite

---
+++ README.md
@@ -0,0 +1,3 @@
+The movie database
+
+A simple application to search and view movies from omdbapi.com open API.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      THE MOVIE DATABASE
+    </div>
+  )
+}
+
+export default App
