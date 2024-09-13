9addda8f923b63bbdf1368576064ab7c911cef8c
Jim Nervall <nervall@hotmail.com>
2024-09-13 12:57:18 +0200

    Setting up test environment with Vitest and React Testing library
    Vitest - Works good with Vite dev server. Sharing a common
    transformation pipeline during dev, build, and test time.
    
    Other plugins
    @testing-library/react - is for testing React components
    @testing-library/jest-dom is for DOM assertions
    jsdom - For simulating browser environment in Node for testing
    
    This commit also including som configuration in tsconfig.app.json
    and vite.config.ts to make the test work with Vite and Typescript.
    
    Also added file setupTests.ts to bring Jest's DOM matchers into Vite,
    making testing feel more familiar with Jest.

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import { screen, render } from "@testing-library/react";
+import App from "./App";
+
+describe("App tests", () => {
+  it("should render the title", () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole("heading")
+    ).toHaveTextContent("THE MOVIE DATABASE");
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ function App() {
 
 
   return (
-    <div>
+    <h1>
       THE MOVIE DATABASE
-    </div>
+    </h1>
   )
 }
 
