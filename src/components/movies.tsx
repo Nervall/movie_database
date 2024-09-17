94945e6d9295a92b380744cf3838c123bcfb5cf9
Jim Nervall <nervall@hotmail.com>
2024-09-17 16:57:20 +0200

    Added component Movies.tsx
    The component pick search string from params in url and makes an
    api request to OMDB api and render result or Error. Also added the
    Route to App.tsx and react-routers navigate in the Header.tsx to be
    able to route to Movies.tsx.

---
+++ src/components/movies.tsx
@@ -0,0 +1,61 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { apiRequest } from '../utils/api';
+import './movies.css';
+
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const { search } = useParams();
+
+  const handleSearch = async (searchValue: string) => {
+    if (searchValue.trim().toLowerCase()) {
+        try {
+            const movies = await apiRequest(searchValue, 'SEARCH');
+            if (movies.Response === 'True') {
+            setMovies(movies.Search);
+            } else {
+            setError('No movies found');
+            }
+        } catch (err) {
+            setError('Failed to fetch movies');
+        } finally {
+            setLoading(false);
+        }
+    }
+  };
+
+  useEffect(() => {
+    if (search) {
+        setLoading(true);
+        setError('');
+        handleSearch(search)
+    }
+  }, []);
+
+  return (
+    <div className='movies-wrapper'>
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {movies.length > 0 && 
+            <ul>
+                {movies.map((movie: any) => (
+                    <li key={movie.imdbID}>
+                        <button className='movies-button'>
+                        <img src={movie.Poster} alt={movie.Title} />
+                        <div className='movies-information'>
+                            <h3>{movie.Title} ({movie.Year})</h3>
+                            <p>{`Year: ${movie.Year}`}</p>
+                            <p>{`Type: ${movie.Type}`}</p>
+                        </div>
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        }
+    </div>
+  )
+};
+
+export default Movies;
