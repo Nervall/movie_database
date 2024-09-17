d700aca1c1178b308b2ed9085282f125c302c582
Jim Nervall <nervall@hotmail.com>
2024-09-17 15:59:10 +0200

    Installed dotenv package and Added API function to handle requests to OMDB API
    The API handles 3 types of requests (for now):
    SEARCH - search for movies with string
    ID - search for movies with IMDB ID
    TITLE - search for movies with title string

---
+++ src/utils/api.ts
@@ -0,0 +1,30 @@
+const API_URL = 'https://www.omdbapi.com/';
+const API_KEY =  import.meta.env.VITE_API_KEY;
+
+type TypeOfSearch = "TITLE" | "ID" | "SEARCH";
+
+const typeOfSearch = (query:string, type: TypeOfSearch) => {
+    switch (type) {
+        case 'TITLE':
+           return `&t=${query}`;
+        case "ID":
+            return `&i=${query}`
+        default:
+           return `&s=${query}`
+    }
+}
+
+export const apiRequest = async (query: string, type: TypeOfSearch) => {
+    const search = typeOfSearch(query, type)
+    try {
+        const response = await fetch(`${API_URL}?apikey=${API_KEY}${search}`);
+        if (!response.ok) {
+        throw new Error('Something with the network response went wrong');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        throw new Error('Failed to fetch movies');
+    }
+};
