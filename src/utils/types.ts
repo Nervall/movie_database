5a82540c24937826c6227d7f043bb9519d8ba6c9
Jim Nervall <nervall@hotmail.com>
2024-09-17 18:00:42 +0200

    Added types for Movie details

---
+++ src/utils/types.ts
@@ -0,0 +1,20 @@
+export type MovieDetailsType = {
+    Title: string;
+    Year: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Awards: string;
+    Poster: string;
+    imdbRating: string;
+    imdbVotes: string;
+    totalSeasons: string;
+    Type: string;
+}
