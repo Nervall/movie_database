c00cdce158dae5153719955f272ce7ebc2ce2dda
Jim Nervall <nervall@hotmail.com>
2024-09-17 15:06:03 +0200

    Added searchInput component
    The searchInput component is a reusable component that handles
    search. It's a formelement with submit and an input element.
    
    The component task is to give you a search string from user.
    You get the string through props function handleSearch.

---
+++ src/components/searchInput.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+import SearchInput from './searchInput';
+
+describe('SearchInput Component', () => {
+  it('renders the search input', () => {
+    render(<SearchInput handleSearch={vi.fn()} />);
+
+    expect(screen.getByPlaceholderText(/search/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /search/i })).toBeInTheDocument();
+    expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+
+  it('updates the input value on typing', () => {
+    render(<SearchInput handleSearch={vi.fn()} />);
+
+    const input = screen.getByPlaceholderText(/search/i);
+    
+    fireEvent.change(input, { target: { value: 'inception' } });
+    expect(input).toHaveValue('inception');
+  });
+
+  it('calls handleSearch value when form is submitted', () => {
+    const mockHandleSearch = vi.fn();
+    
+    render(<SearchInput handleSearch={mockHandleSearch} />);
+
+    const input = screen.getByPlaceholderText('Search...');
+    const form = screen.getByRole('form');
+
+    fireEvent.change(input, { target: { value: 'inception' } });
+    fireEvent.submit(form);
+
+    expect(mockHandleSearch).toHaveBeenCalledWith('inception');
+  });
+});
---
+++ src/components/searchInput.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import SearchIcon from '../assets/searchicon.svg';
+import './searchInput.css';
+
+type SearchInputProps = {
+    name?: string;
+    placeholder?: string;
+    handleSearch: (search: string) => void
+};
+
+const SearchInput = ({ name = "search", placeholder = "Search...", handleSearch }: SearchInputProps) => {
+  const [searchValue, setSearchValue] = useState('');
+
+    return (
+      <form className="search-wrapper" onSubmit={() => handleSearch(searchValue)} role='form'>
+        {SearchIcon && <img src={SearchIcon} alt="Search icon" />}
+        <input
+          className="search-input"
+          id={name}
+          type="search"
+          name={name}
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder={placeholder}
+        />
+        <button type="submit">Search</button>
+      </form>
+    );
+  };
+  
+
+export default SearchInput;
