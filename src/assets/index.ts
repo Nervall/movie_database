e42f35045276bfb6ed7af37c89f3cf74edb9992e
Jim Nervall <nervall@hotmail.com>
2024-09-18 14:35:06 +0200

    Added logo in header.tsx

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,10 +2,12 @@ import Movie1 from './movie1.jpg';
 import Movie2 from './movie2.jpg';
 import Movie3 from './movie3.jpg';
 import Movie4 from './movie4.jpg';
+import Logo from './movie_logo.png';
 
 export {
     Movie1,
     Movie2,
     Movie3,
     Movie4,
+    Logo,
 };
