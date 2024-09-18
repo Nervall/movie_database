fad7a5fa9120d867d51571bc370b9602357f7aa7
Jim Nervall <nervall@hotmail.com>
2024-09-18 13:45:46 +0200

    Added links to movie posters in home component
    Added hardcoded data to route users to correct movie when clicking
    on a movie poster. Should be fetched from an api in the real world.
    Also added related CSS changes.

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import {
     Movie1,
     Movie2,
@@ -7,15 +8,35 @@ import {
 import './home.css';
 
 const Home = () => {
-    const imgArr = [ Movie1, Movie2, Movie3, Movie4 ];
+    // This is hardcoded in this test, data should be fetched from api
+    const movieArr = [ 
+        {
+            img: Movie1,
+            imdbID: "tt3411444",
+        },
+        {
+            img: Movie2,
+            imdbID: "tt0120338",
+        },
+        {
+            img: Movie3,
+            imdbID: "tt3766354",
+        },
+        {
+            img: Movie4,
+            imdbID: "tt2446042",
+        },
+    ];
 
     return (
         <>
             <p className='home-header'>POPULAR MOVIES</p>
             <div className='home-img-wrapper'>
-                {imgArr.map((img) => {
+                {movieArr.map(({img, imdbID}) => {
                     return (
-                        <img className='home-img' src={img} alt="images from Netflix in educational purpose" />
+                        <Link to={"/movie/" + imdbID} key={imdbID} className='home-link'>
+                            <img className='home-img' src={img} alt="images from Netflix in educational purpose" />
+                        </Link>
                     )
                 })}
             </div>
